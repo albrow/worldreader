aa85edf9bb780787ee92de2b910306fa76360d74
albrow <stephenalexbrowne@gmail.com>
2014-11-15 23:56:12 -0500

    Use AsyncTask for posting image to API

---
+++ main/java/info/alexbrowne/eyes/ImageProcessor.java
@@ -0,0 +1,103 @@
+package info.alexbrowne.eyes;
+
+import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by alex on 11/15/14.
+ */
+public class ImageProcessor extends AsyncTask<File, Void, Void> {
+
+    private static String TAG = "ImageProcessor";
+    private final double CONFIDENCE_THRESHOLD = 0.03;
+    private Speaker speaker;
+
+    public ImageProcessor(Speaker speaker) {
+        this.speaker = speaker;
+    }
+
+    @Override
+    protected Void doInBackground(File... imageFile) {
+        String rawData = "";
+        try {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost postRequest = new HttpPost(
+                    "http://www.clarifai.com/demo/upload/");
+            FileBody bin = new FileBody(imageFile[0]);
+            MultipartEntity reqEntity = new MultipartEntity(
+                    HttpMultipartMode.BROWSER_COMPATIBLE);
+            reqEntity.addPart("files[]", bin);
+            postRequest.setEntity(reqEntity);
+            HttpResponse response = httpClient.execute(postRequest);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    response.getEntity().getContent(), "UTF-8"));
+            String sResponse;
+            StringBuilder s = new StringBuilder();
+
+            while ((sResponse = reader.readLine()) != null) {
+                s = s.append(sResponse);
+            }
+            rawData = s.toString();
+            Log.d(TAG, "Response: " + rawData);
+        } catch (Exception e) {
+            // handle exception here
+            Log.e(TAG, "Error posting image file: " + e.getMessage());
+        }
+
+        JSONObject jObject = null;
+        try {
+            jObject = new JSONObject(rawData);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        ArrayList<Guess> topGuesses = new ArrayList<Guess>();
+        try {
+            JSONObject result = jObject.getJSONArray("files").getJSONObject(0);
+            JSONArray values = result.getJSONArray("predicted_classes");
+            JSONArray confidences = result.getJSONArray("predicted_probs");
+            double prevConfidence = 0.0;
+            for (int i = 0; i < values.length(); i++) {
+                Guess guess = new Guess(values.getString(i), confidences.getString(i));
+                double c = guess.getConfidence();
+                if (c >= CONFIDENCE_THRESHOLD && c >= prevConfidence*0.5)  {
+                    // Add to our guesses iff confidence for this guess is over some
+                    // threshold and this guess is at least half as likely
+                    topGuesses.add(guess);
+                    prevConfidence = c;
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Speak the top results
+        Log.d(TAG, "top guesses: " + topGuesses);
+        speaker.allow(true);
+        if (topGuesses.size() == 0) {
+            speaker.speak("Unkown object");
+        } else if (topGuesses.size() == 1) {
+            speaker.speak(topGuesses.get(0).toString());
+        } else {
+            String phrase = TextUtils.join(" or ", topGuesses.subList(0, Math.min(3, topGuesses.size())));
+            speaker.speak(phrase);
+        }
+        return null;
+    }
+}
--- main/java/info/alexbrowne/eyes/MainActivity.java
+++ main/java/info/alexbrowne/eyes/MainActivity.java
@@ -7,9 +7,7 @@ import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.StrictMode;
 import android.speech.tts.TextToSpeech;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,25 +15,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends Activity {
@@ -50,18 +34,15 @@ public class MainActivity extends Activity {
 
     private Speaker speaker;
 
-    private final double CONFIDENCE_THRESHOLD = 0.03;
-
-    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StrictMode.setThreadPolicy(policy);
 
         setContentView(R.layout.activity_main);
 
         Context mContext = getApplicationContext();
+
+        this.speaker = new Speaker(mContext);
 
         // Check if we can access the camera
         if (!checkCameraHardware(mContext)) {
@@ -167,7 +148,7 @@ public class MainActivity extends Activity {
                 FileOutputStream fos = new FileOutputStream(pictureFile);
                 fos.write(data);
                 fos.close();
-                postImage(pictureFile);
+                new ImageProcessor(speaker).execute(pictureFile);
             } catch (FileNotFoundException e) {
                 Log.d(TAG, "File not found: " + e.getMessage());
             } catch (IOException e) {
@@ -226,72 +207,6 @@ public class MainActivity extends Activity {
         if (mCamera != null){
             mCamera.release();        // release the camera for other applications
             mCamera = null;
-        }
-    }
-
-    private void postImage(File imageFile) {
-        String rawData = "";
-        try {
-            HttpClient httpClient = new DefaultHttpClient();
-            HttpPost postRequest = new HttpPost(
-                    "http://www.clarifai.com/demo/upload/");
-            FileBody bin = new FileBody(imageFile);
-            MultipartEntity reqEntity = new MultipartEntity(
-                    HttpMultipartMode.BROWSER_COMPATIBLE);
-            reqEntity.addPart("files[]", bin);
-            postRequest.setEntity(reqEntity);
-            HttpResponse response = httpClient.execute(postRequest);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    response.getEntity().getContent(), "UTF-8"));
-            String sResponse;
-            StringBuilder s = new StringBuilder();
-
-            while ((sResponse = reader.readLine()) != null) {
-                s = s.append(sResponse);
-            }
-            rawData = s.toString();
-            Log.d(TAG, "Response: " + rawData);
-        } catch (Exception e) {
-            // handle exception here
-            Log.e(TAG, "Error posting image file: " + e.getMessage());
-        }
-
-        JSONObject jObject = null;
-        try {
-            jObject = new JSONObject(rawData);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        ArrayList<Guess> topGuesses = new ArrayList<Guess>();
-        try {
-            JSONObject result = jObject.getJSONArray("files").getJSONObject(0);
-            JSONArray values = result.getJSONArray("predicted_classes");
-            JSONArray confidences = result.getJSONArray("predicted_probs");
-            double prevConfidence = 0.0;
-            for (int i = 0; i < values.length(); i++) {
-                Guess guess = new Guess(values.getString(i), confidences.getString(i));
-                double c = guess.getConfidence();
-                if (c >= CONFIDENCE_THRESHOLD && c >= prevConfidence*0.5)  {
-                    // Add to our guesses iff confidence for this guess is over some
-                    // threshold and this guess is at least half as likely 
-                    topGuesses.add(guess);
-                    prevConfidence = c;
-                }
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        // Speak the top results
-        Log.d(TAG, "top guesses: " + topGuesses);
-        speaker.allow(true);
-        if (topGuesses.size() == 0) {
-            speaker.speak("Unkown object");
-        } else if (topGuesses.size() == 1) {
-            speaker.speak(topGuesses.get(0).toString());
-        } else {
-            String phrase = TextUtils.join(" or ", topGuesses.subList(0, Math.min(3, topGuesses.size())));
-            speaker.speak(phrase);
         }
     }
 }
