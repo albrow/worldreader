f920cd5d523ba274257af64101c34f31bb3b4b63
albrow <stephenalexbrowne@gmail.com>
2014-11-16 03:44:00 -0500

    Keep screen on

--- main/java/info/alexbrowne/eyes/MainActivity.java
+++ main/java/info/alexbrowne/eyes/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends Activity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         Log.i(TAG, "onCreate");
 
