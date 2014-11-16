9fd8c81e163cfefc39fca96c63b9e87b501fd8ba
albrow <stephenalexbrowne@gmail.com>
2014-11-16 00:07:21 -0500

    Allow for continuous pictures.

--- main/java/info/alexbrowne/eyes/MainActivity.java
+++ main/java/info/alexbrowne/eyes/MainActivity.java
@@ -99,21 +99,6 @@ public class MainActivity extends Activity {
         releaseCamera();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /** Check if this device has a camera */
     private boolean checkCameraHardware(Context context) {
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
@@ -146,6 +131,7 @@ public class MainActivity extends Activity {
             } catch (IOException e) {
                 Log.d(TAG, "Error accessing file: " + e.getMessage());
             }
+            camera.startPreview();
         }
     };
 
